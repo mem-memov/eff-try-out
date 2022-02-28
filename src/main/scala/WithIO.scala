5c4a27459b1eda4786b3e225c8b0359eb864456d
Alexander Novikov <anovikov@tsum.ru>
2022-02-28 17:08:15 +0300

    working!

--- src/main/scala/WithIO.scala
+++ src/main/scala/WithIO.scala
@@ -4,26 +4,26 @@ import org.atnos.eff.syntax.all._
 import org.atnos.eff.addon.cats.effect._
 import org.atnos.eff.syntax.addon.cats.effect._
 
-
 object WithIO extends IOApp.Simple {
   type AppStack = Fx.fx1[IO[_]]
   import cats.effect.unsafe.implicits.global
 
-  override def run: IO[Unit] =
-
+  override def run: IO[Unit] = {
     val program: Eff[AppStack, Unit] =
       for
-        _ <- printLn[AppStack]("Hello Eff!")
-      yield ()
+        r <- printLn[AppStack]("Hello Eff!")
+      yield r
 
     program
       .unsafeRunSync(global)
 
+    IO.unit
+  }
 
-    type _io[R] = MemberIn[IO[_], R]
+  type _io[R] = MemberIn[IO[_], R]
 
-    def printLn[R: _io](text: String): Eff[R, Unit] = {
-      val action: IO[Unit] = IO.delay(println("1111"))
-      IOEffect.fromIO(action)
-    }
+  def printLn[R: _io](text: String): Eff[R, Unit] = {
+    val action: IO[Unit] = IO.delay(println("1111"))
+    IOEffect.fromIO[R, Unit](action)
+  }
 }
